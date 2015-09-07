404fb586905a741037f9462805c6c5a9bf673e87
Ingar Arntzen <ingar.arntzen@gmail.com>
2015-09-07 16:54:25 +0200

    changed build target to build/built

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
-node r.js -o baseUrl=../Sequencer name=sequencer out=../lib/sequencer-require-min.js # built and minified for requirejs usage
-node r.js -o baseUrl=../Sequencer name=sequencer optimize=none out=../lib/sequencer-require.js # built for requirejs usage
-node r.js -o almond-build.js optimize=none out=../lib/sequencer.js
-node r.js -o almond-build.js out=../lib/sequencer-min.js
+node r.js -o baseUrl=../Sequencer name=sequencer out=built/sequencer-require-min.js # built and minified for requirejs usage
+node r.js -o baseUrl=../Sequencer name=sequencer optimize=none out=built/sequencer-require.js # built for requirejs usage
+node r.js -o almond-build.js optimize=none out=built/sequencer.js # built for regular script includes
+node r.js -o almond-build.js out=built/sequencer-min.js # built and minified for regular script includes
